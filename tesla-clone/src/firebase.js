a4b0b6cc47344c5683aaad70571db27f40bda1db
Austin Pierce <Austinentpierce@gmail.com>
2022-04-30 16:22:00 -0400

    adding files

---
+++ tesla-clone/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjtgI3LaBz7Hz03K5reVgk9eg6XJrPqq0",
+  authDomain: "tesla-clone-2ec7f.firebaseapp.com",
+  projectId: "tesla-clone-2ec7f",
+  storageBucket: "tesla-clone-2ec7f.appspot.com",
+  messagingSenderId: "273955356979",
+  appId: "1:273955356979:web:86fe3291010812dc10be46",
+  measurementId: "G-9L55JWELHJ",
+};
+
+const app = initializeApp(firebaseConfig);
